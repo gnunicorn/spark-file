12a0ed3d02bddbf8d41ea12bda9d799f46c06354
Benjamin Kampmann <ben.kampmann@gmail.com>
2014-06-08 19:24:28 +0200

    this. is. SPARK FILE.

--- www/js/app.js
+++ www/js/app.js
@@ -3,54 +3,33 @@
 // backbone hoodie connector go!
 Backbone.hoodie.connect();
 
-var Todo = Backbone.Model.extend({
+var Spark = Backbone.Model.extend({
 
   // backbone-hoodie needs this
-  type: 'todo',
-  
-  // Default attributes for the todo
-  // and ensure that each todo created has `title` and `completed` keys.
+  type: 'spark',
+
+  // Default attributes for the Spark
   defaults: {
-    title: '',
-    completed: false
+    text: '',
+    when: null,
+    last_edit: null
   },
 
-  // Toggle the `completed` state of this todo item.
-  toggle: function() {
-    this.save({
-      completed: !this.get('completed')
-    });
+  initialize: function() {
+    if(!this.has('when')) {
+        this.set('when', moment());
+    }
   }
 });
 
-var TodoList = Backbone.Collection.extend({
+var SparkList = Backbone.Collection.extend({
 
   // Reference to this collection's model.
-  model: Todo,
+  model: Spark,
 
-  // Filter down the list of all todo items that are finished.
-  completed: function() {
-    return this.filter(function( todo ) {
-      return todo.get('completed');
-    });
-  },
-
-  remaining: function() {
-    return this.without.apply(this, this.completed());
-  },
-
-  // We keep the Todos in sequential order, despite being saved by unordered
-  // GUID in the database. This generates the next order number for new items.
-  nextOrder: function () {
-    if (!this.length) {
-      return 1;
-    }
-    return this.last().get('order') + 1;
-  },
-
-  // Todos are sorted by their original insertion order.
-  comparator: function (todo) {
-    return todo.get('order');
+  // sparks are sorted by their original insertion order.
+  comparator: function (spark) {
+    return - moment(spark.get('when')).diff(moment());
   }
 });
 
@@ -75,76 +54,73 @@ var Utils = {
 
 // Begin React stuff
 
-var TodoItem = React.createClass({
+var SparkItem = React.createClass({
   handleSubmit: function(event) {
     var val = this.refs.editField.getDOMNode().value.trim();
     if (val) {
-      this.props.onSave(val);
+      this.props.spark.set({text: val, last_edit: moment()});
+      this.props.spark.save();
+      this.setState({"editing": false});
     } else {
       this.props.onDestroy();
     }
     return false;
   },
+  getInitialState: function() {
+    return {expanded: false, editing: false};
+  },
 
-  onEdit: function() {
-    this.props.onEdit();
+  expand: function() {
+    this.setState({"expanded": true});
+  },
+
+  edit: function() {
+    this.setState({"editing": true});
     this.refs.editField.getDOMNode().focus();
   },
 
+  cancel: function() {
+    this.setState({"editing": false});
+  },
+
+  save: function(spark, text) {
+    spark.set('text', text);
+    this.setState({editing: null});
+  },
+
+
   render: function() {
     var classes = Utils.stringifyObjKeys({
-      completed: this.props.todo.get('completed'), editing: this.props.editing
+      expanded: this.state.expanded, editing: this.state.editing
     });
     return (
       <li className={classes}>
         <div className="view">
-          <input
-            className="toggle"
-            type="checkbox"
-            checked={this.props.todo.get('completed')}
-            onChange={this.props.onToggle}
-            key={this.props.key}
-          />
-          <label onDoubleClick={this.onEdit}>
-            {this.props.todo.get('title')}
-          </label>
-          <button className="destroy" onClick={this.props.onDestroy} />
+          <span className="when">{moment(this.props.spark.when).fromNow()}</span>
+          <p className="spark" onClick={this.expand}>
+            {this.props.spark.get('text')}
+          </p>
+          <button onClick={this.edit}>
+            <span className="fa fa-pencil-square-o"></span>
+          </button>
+          <button onClick={this.props.onDestroy}>
+            <span className="fa fa-trash-o"></span>
+          </button>
         </div>
         <form onSubmit={this.handleSubmit}>
-          <input
+          <textarea
             ref="editField"
             className="edit"
-            defaultValue={this.props.todo.get('title')}
-            onBlur={this.handleSubmit}
-            autoFocus="autofocus"
-          />
+            defaultValue={this.props.spark.get('text')}
+            autoFocus="autofocus" />
+          <button onClick={this.handleSubmit}>
+            <span className="fa fa-check"></span>
+          </button>
+          <button onClick={this.cancel}>
+            <span className="fa fa-times"></span>
+          </button>
         </form>
       </li>
-    );
-  }
-});
-
-var TodoFooter = React.createClass({
-  render: function() {
-    var activeTodoWord = Utils.pluralize(this.props.count, 'todo');
-    var clearButton = null;
-
-    if (this.props.completedCount > 0) {
-      clearButton = (
-        <button id="clear-completed" onClick={this.props.onClearCompleted}>
-          Clear completed ({this.props.completedCount})
-        </button>
-      );
-    }
-
-    return (
-      <footer id="footer">
-        <span id="todo-count">
-          <strong>{this.props.count}</strong>{' '}
-          {activeTodoWord}{' '}left
-        </span>
-        {clearButton}
-      </footer>
     );
   }
 });
@@ -174,15 +150,14 @@ var BackboneMixin = {
   }
 };
 
-var TodoApp = React.createClass({
+var SparkApp = React.createClass({
   mixins: [BackboneMixin],
   getInitialState: function() {
-    return {editing: null};
+    return {editing: null, expanded: false};
   },
 
   componentDidMount: function() {
-    // Additional functionality for todomvc: fetch() the collection on init
-    this.props.todos.fetch();
+    this.props.sparks.fetch();
     this.refs.newField.getDOMNode().focus();
   },
 
@@ -190,84 +165,50 @@ var TodoApp = React.createClass({
     // If saving were expensive we'd listen for mutation events on Backbone and
     // do this manually. however, since saving isn't expensive this is an
     // elegant way to keep it reactively up-to-date.
-    this.props.todos.forEach(function(todo) {
-      todo.save();
+    this.props.spark.forEach(function(spark) {
+      spark.save();
     });
   },
 
   getBackboneModels: function() {
-    return [this.props.todos];
+    return [this.props.sparks];
   },
 
   handleSubmit: function(event) {
     event.preventDefault();
     var val = this.refs.newField.getDOMNode().value.trim();
     if (val) {
-      this.props.todos.create({
-        title: val,
-        completed: false,
-        order: this.props.todos.nextOrder()
+      this.props.sparks.create({
+        text: val,
+        when: moment(),
+        last_edit: moment()
       });
       this.refs.newField.getDOMNode().value = '';
     }
   },
 
-  toggleAll: function(event) {
-    var checked = event.nativeEvent.target.checked;
-    this.props.todos.forEach(function(todo) {
-      todo.set('completed', checked);
-    });
-  },
-
-  edit: function(todo) {
-    this.setState({editing: todo.get('id')});
-  },
-
-  save: function(todo, text) {
-    todo.set('title', text);
-    this.setState({editing: null});
-  },
-
   clearCompleted: function() {
-    this.props.todos.completed().forEach(function(todo) {
-      todo.destroy();
+    this.props.sparks.completed().forEach(function(spark) {
+      spark.destroy();
     });
   },
 
   render: function() {
-    var footer = null;
-    var main = null;
-    var todoItems = this.props.todos.map(function(todo) {
-      return (
-        <TodoItem
-          key={Math.random()}
-          todo={todo}
-          onToggle={todo.toggle.bind(todo)}
-          onDestroy={todo.destroy.bind(todo)}
-          onEdit={this.edit.bind(this, todo)}
-          editing={this.state.editing === todo.get('id')}
-          onSave={this.save.bind(this, todo)}
-        />
-      );
-    }, this);
+    var main = null,
+        footer = null,
+        sparkItems = this.props.sparks.map(function(spark) {
+          return (
+            <SparkItem
+              key={spark.id}
+              spark={spark} />
+          );
+        }, this);
 
-    var activeTodoCount = this.props.todos.remaining().length;
-    var completedCount = todoItems.length - activeTodoCount;
-    if (activeTodoCount || completedCount) {
-      footer =
-        <TodoFooter
-          count={activeTodoCount}
-          completedCount={completedCount}
-          onClearCompleted={this.clearCompleted}
-        />;
-    }
-
-    if (todoItems.length) {
+    if (sparkItems.length) {
       main = (
         <section id="main">
-          <input id="toggle-all" type="checkbox" onChange={this.toggleAll} />
-          <ul id="todo-list">
-            {todoItems}
+          <ul id="spark-list">
+            {sparkItems}
           </ul>
         </section>
       );
@@ -275,14 +216,14 @@ var TodoApp = React.createClass({
 
     return (
       <div>
-        <section id="todoapp">
+        <section id="sparkapp">
           <header id="header">
-            <h1>todos</h1>
+            <h1>sparks</h1>
             <form onSubmit={this.handleSubmit}>
               <input
                 ref="newField"
-                id="new-todo"
-                placeholder="What needs to be done?"
+                id="new-spark"
+                placeholder="record your spark"
               />
             </form>
           </header>
@@ -290,14 +231,14 @@ var TodoApp = React.createClass({
           {footer}
         </section>
         <footer id="info">
-          <p>Double-click to edit a todo</p>
+          <p>Double-click to edit a spark</p>
           <p>
             Created by{' '}
             <a href="http://github.com/svnlto/">svnlto</a>
             based on the work of
             <a href="http://github.com/petehunt/">petehunt</a>
           </p>
-          <p>Part of{' '}<a href="http://todomvc.com">TodoMVC</a></p>
+          <p>Part of{' '}<a href="http://sparkmvc.com">sparkMVC</a></p>
         </footer>
       </div>
     );
@@ -305,5 +246,5 @@ var TodoApp = React.createClass({
 });
 
 React.renderComponent(
-  <TodoApp todos={new TodoList()} />, document.getElementById('container')
+  <SparkApp sparks={new SparkList()} />, document.getElementById('container')
 );
